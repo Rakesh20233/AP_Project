4cc90197de64a43551b6edfc6d41e3e0b3ca7621
unknown <Rakesh20233@iiitd.ac.in>
2021-12-14 13:01:55 +0530

    Files by Rakesh

--- JavaFX_using_SceneBuilder/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javafx_using_scenebuilder {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javafx_using_scenebuilder to javafx.fxml;
-    exports com.example.javafx_using_scenebuilder;
-}
